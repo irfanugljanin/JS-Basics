c986bd74fcfd61599bc0f26dbaa8a8080fdbec4b
irfan ugljanin <irfanikresoft@gmail.com>
2022-03-21 19:14:15 +0100

    Operator Precedence

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ console.log(fistName);
 // lastName = "Schmedtmann";
 // console.log(lasName);
 
+/*
 //Math operators
 const now = 2037;
 const ageJonas = now - 1991;
@@ -70,3 +71,19 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+// console.log(25-10-5);
+
+let x,
+  y: x = (y = 25 - 10 - 5); //x=y=10, x=10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
