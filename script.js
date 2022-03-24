e4446db70557e817ad5a519eeb668cc824baf776
irfan ugljanin <irfanikresoft@gmail.com>
2022-03-24 17:54:30 +0100

    Arrow Functions

--- script.js
+++ script.js
@@ -408,16 +408,28 @@ if (hasDriversLicence && hasGoodVision && !isTired) {
 // const appleJuice = fruitProcessor(5, 0);
 // console.log(appleJuice);
 
-//function expression
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
-const age1 = calcAge1(1991);
+// //function expression
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
 
-//function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
+// //function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+//arrow functions
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
 };
-const age2 = calcAge2(1991);
-
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991, "Jonas"));
