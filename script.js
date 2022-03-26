84235a83de823a71ab487330630cb456e3c2aa57
irfan ugljanin <irfanikresoft@gmail.com>
2022-03-26 17:47:01 +0100

    Objects

--- script.js
+++ script.js
@@ -568,12 +568,52 @@ if (hasDriversLicence && hasGoodVision && !isTired) {
 //   console.log("You have aa friend called Steven");
 // }
 
-function calcTip(bill) {
-  const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
-  return tip;
+//challenge
+// function calcTip(bill) {
+//   const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+//   return tip;
+// }
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(bills, tips, totals);
+
+//objects
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends"
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log(
+    "Wrong request! Choose between firstName, lastName, age, job and friends"
+  );
 }
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(bills, tips, totals);
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschedtmann";
+console.log(jonas);
+
+//challenge
+//'Jonas has 3 friends, and his best friend is called Michael'
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);
