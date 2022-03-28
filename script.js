c634ac95f515972c82602fc97b2abd74a7c4e087
irfan ugljanin <irfanikresoft@gmail.com>
2022-03-28 20:26:44 +0200

    Challenge

--- script.js
+++ script.js
@@ -783,16 +783,40 @@ if (hasDriversLicence && hasGoodVision && !isTired) {
 // }
 
 //while loop
-let rep = 1;
-while (rep <= 10) {
-  // console.log(`WHILE: Lifting weights repetition ${rep}`);
-  rep++;
+// let rep = 1;
+// while (rep <= 10) {
+//   // console.log(`WHILE: Lifting weights repetition ${rep}`);
+//   rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log("Loop is about to end...");
+// }
+
+function calcTip(bill) {
+  return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log("Loop is about to end...");
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
 }
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+    return sum / arr.length;
+  }
+};
