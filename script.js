1d4e627d352e50d7ecd36695aa4d4deb53af82ee
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-03-30 18:35:55 +0200

    Challenge

--- script.js
+++ script.js
@@ -797,26 +797,63 @@ if (hasDriversLicence && hasGoodVision && !isTired) {
 //   if (dice === 6) console.log("Loop is about to end...");
 // }
 
-function calcTip(bill) {
-  return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
-}
+// function calcTip(bill) {
+//   return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// }
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
 
-for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
-}
-console.log(bills, tips, totals);
+// for (let i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
+// console.log(bills, tips, totals);
 
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    // sum = sum + arr[i];
-    sum += arr[i];
-    return sum / arr.length;
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     // sum = sum + arr[i];
+//     sum += arr[i];
+//     return sum / arr.length;
+//   }
+// };
+
+const temperatures = [3 - 2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
   }
+  console.log(max, min);
+  return max - min;
 };
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([1, 5, 7], [(9, 1, 5)]);
+console.log(amplitudeNew);
