015bea56f555599d20c0877e5b2fae54941205b9
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-04-02 16:05:43 +0200

    Challenge

--- script.js
+++ script.js
@@ -821,39 +821,67 @@ if (hasDriversLicence && hasGoodVision && !isTired) {
 //   }
 // };
 
-const temperatures = [3 - 2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3 - 2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-const calcTempAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitudeNew = calcTempAmplitudeNew([1, 5, 7], [(9, 1, 5)]);
+// console.log(amplitudeNew);
+
+// const measurKelvin = function () {
+//   const measurement = {
+//     type: "temp",
+//     unit: "celsius",
+//     value: Number(prompt("Deegres celsius:")),
+//   };
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// console.log(measurKelvin());
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`...${data1[0]}C ...${data1[1]}C ...${data1[2]}C`);
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}C in ${i + 1} days ... `;
   }
-  console.log(max, min);
-  return max - min;
+  console.log("..." + str);
 };
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
 
-const calcTempAmplitudeNew = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
-
-  let max = temps[0];
-  let min = temps[0];
-
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-const amplitudeNew = calcTempAmplitudeNew([1, 5, 7], [(9, 1, 5)]);
-console.log(amplitudeNew);
+printForecast(data1);
