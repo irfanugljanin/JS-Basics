de5a0f842544e109156ab4d403fcee1c5ca6a07e
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-04-25 17:11:08 +0200

    destructuring arrays

--- 05/script.js
+++ 05/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // const calcAge = function (birthYear) {
 //   console.log(2037 - birthYear);
 // };
@@ -13,35 +15,80 @@
 // jonas.calcAge();
 
 // Primitive types
-let lastName = "Williams";
-let oldLastName = lastName;
-lastName = "Davis";
-console.log(lastName, oldLastName);
+// let lastName = "Williams";
+// let oldLastName = lastName;
+// lastName = "Davis";
+// console.log(lastName, oldLastName);
 
-//Reference types
-const jessica = {
-  firstName: "Jessica",
-  lastName: "Williams",
-  age: 27,
+// //Reference types
+// const jessica = {
+//   firstName: "Jessica",
+//   lastName: "Williams",
+//   age: 27,
+// };
+
+// const marriedJessica = jessica;
+// marriedJessica.lastName = "Davis";
+// console.log("Before merriage:", jessica);
+// console.log("After merriage:", marriedJessica);
+
+// // marriedJessica = {};
+
+// // Copying objects
+
+// const jessica2 = {
+//   firstName: "Jessica",
+//   lastName: "Williams",
+//   age: 27,
+//   family: ["Alice", "Bob"],
+// };
+
+// const jessicaCopy = Object.assign({}, jessica2);
+// jessicaCopy.lastName = "Davis";
+// console.log("Before merriage:", jessica2);
+// console.log("After merriage:", jessicaCopy);
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
 
-const marriedJessica = jessica;
-marriedJessica.lastName = "Davis";
-console.log("Before merriage:", jessica);
-console.log("After merriage:", marriedJessica);
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 
-// marriedJessica = {};
+const [x, y, z] = arr;
+console.log(x, y, z);
 
-// Copying objects
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
 
-const jessica2 = {
-  firstName: "Jessica",
-  lastName: "Williams",
-  age: 27,
-  family: ["Alice", "Bob"],
-};
+//Swtiching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
 
-const jessicaCopy = Object.assign({}, jessica2);
-jessicaCopy.lastName = "Davis";
-console.log("Before merriage:", jessica2);
-console.log("After merriage:", jessicaCopy);
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//recieve 2 return values from function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//default values
+const [p, q, r] = [8, 9];
+console.log(p, q, r);
