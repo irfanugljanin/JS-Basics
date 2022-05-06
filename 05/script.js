31d637fa23018e988f51c63a979f8bc469b58e4a
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-05-06 19:40:39 +0200

    practicing

--- 05/script.js
+++ 05/script.js
@@ -48,28 +48,38 @@
 // console.log("Before merriage:", jessica2);
 // console.log("After merriage:", jessicaCopy);
 
+//ES6 ENHANCED OBJECT LITERALS
+//WE CAN PUT ANY VARIABLE INSIDE ANY OBJECT WE JUST NEED TO WRITE EXACT VARIABLE NAME INTO OBJECT AND
+//IT WILL IMMEDIATLEY PUT IT AS PROPERTY
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const openingHours = {
+  [weekdays[3]]: {
+    //instead of writing 'thu' we can do this
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 
-  order: function (starterIndex, mainIndex) {
+  //object literals(putting variable into object as property)
+  openingHours,
+
+  //es6 feature - you can remove 'function' keyword and semicolon(:)
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -90,6 +100,157 @@ const restaurant = {
   //   console.log(`Here is ur delicious pasta with ${ing1}, ${ing2}, ${ing3}.`);
   // },
 };
+
+//PROPERTIE NAMES
+const properties = Object.keys(openingHours);
+let openStr = `We are open on ${properties.length} days:`;
+for (const day of properties) {
+  openStr += `${day},`;
+}
+
+//PROPERTIE VALUE
+const values = Object.values(openingHours);
+
+//LOOPING THROUGH ENTIRE OBJECT
+const entries = Object.entries(openingHours);
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close ${close}`);
+}
+/*
+//////////////////////////////
+//---OPTIONAL CHAINING ---
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+//USING THE QUESTION MARK
+restaurant.openingHours.mon?.open;
+//IF PROPERTY BEFORE QUESTION MARK EXIST THEN THE 'open' PROPERTY WILL BE READ (ELSE UNDEFINED)
+
+//EXAMPLE
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+}
+
+//OPTIONAL CHAINING ALSO WORKS WITH METHODS
+restaurant.order?.(0, 1) ?? "Method does not exist";
+
+//OPTIONAL CHANING WITH ARRAYS
+const users = [{ name: "Jonas", email: "jonas@gmail.com" }];
+users[0]?.name ?? "Users array empty";
+*/
+
+/////////////////////////////////////////////
+/*
+//----FOR OF LOOP ----
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+//INSTEAD OF THIS
+for (const item of menu.entries()) {
+  console.log(`${item[0] + 1}. ${item[1]}`);
+}
+
+
+//USE THIS
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}. ${el}`);
+}
+*/
+
+//---OR ASSIGNMENT LOGICAL OPERATOR
+//WORKS IF VALUES IN OBJECT ARE NOT FALSY VALUES
+
+/*
+const rest1 = {
+  name: "Caprri",
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni",
+};
+
+//INSTEAD OF THIS
+rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+
+//USE THIS
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+*/
+
+//IF WE HAVE FALSY VALUE IN OBJECT WE USE THEN
+//---- NULLISH ASSIGNMENT LOGICAL OPERATOR
+/*
+const rest1 = {
+  name: "Caprri",
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni",
+};
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+*/
+
+/*
+//----AND ASSIGNMENT OPERATOR -----
+// USING IF WE WANNA REPLACE STRINGS
+
+const rest1 = {
+  name: "Caprri",
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni",
+};
+
+//INSTEAD OF THIS
+rest1.owner = rest1.owner && "Anonymous";
+rest2.owner = rest2.owner && "Anonymous";
+
+//USE THIS
+rest1.owner &&= "Anonymous";
+rest2.owner &&= "Anonymous";
+*/
+
+/*
+//---- OR OPERATOR ----
+//OR OPERATOR ALWAYS RETURNS FIRST TRUTHY VALUE
+//IN CASE WERE EVERY VALUE IS FALSIE OR OPERATOR USE LAST VALUE
+
+//PRACTICAL EXAMPLE
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+
+// const guests2 = restaurant.numGuests || 10;
+
+//---- AND OPERATOR ----
+//WORKS OPOSITE OF OR OPERATOR
+
+//PRACTICAL EXPAMPLE
+if (restaurant.orderPizza) {
+  restaurant.orderPizza("mushrooms", "spinach");
+}
+
+restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
+
+//----NULLISH OPERATOR-----
+//
+
+
+restaurant.numGuests = 0;
+const guests2 = restaurant.numGuests || 10;
+
+const guestCorrect = restaurant.numGuests ?? 10;
 
 // --- REST OPERATOR - COLLECTING MULTIPLE ELEMENTS TO ONE ARRAY ---
 //  WE ARE USING REST OPERATOR ON RIGHT SIDE OF EQUAL SIGN
