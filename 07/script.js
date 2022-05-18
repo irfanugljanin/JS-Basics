4924fb7f7b1e1a584ff36a636767c0e254558ede
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-05-18 19:53:48 +0200

    starting new project

--- 07/script.js
+++ 07/script.js
@@ -1,25 +1,31 @@
-// "use strict";
+"use strict";
 
-// console.log("start");
+console.log("start");
 
-// function loginUser(email, password, callback) {
-//   setTimeout(() => {
-//     console.log("now we have the data");
-//     callback({ userEmail: email });
-//   }, 3000);
-// }
+function loginUser(email, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("now we have the data");
+      resolve({ userEmail: email });
+    }, 3000);
+  });
+}
 
-// function getUserVideos(email, callback) {
-//   setTimeout(() => {
-//     callback(["video1", "video2", "video3"]);
-//   }, 2000);
-// }
+function getUserVideos(email) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      callback(["video1", "video2", "video3"]);
+    }, 2000);
+  });
+}
 
-// function videoDetails(video, callback) {
-//   setTimeout(() => {
-//     callback("title of the video");
-//   }, 2000);
-// }
+function videoDetails(video) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("title of the video");
+    }, 2000);
+  });
+}
 
 // const user = loginUser("dev@gmail.com", 1235, (user) => {
 //   console.log(user);
@@ -31,8 +37,14 @@
 //   });
 // });
 
-// console.log("finish");
+loginUser("ed", "asdasd")
+  .then((user) => getUserVideos(user.email))
+  .then((videos) => videoDetails(videos[0]))
+  .then((detail) => console.log(detail));
 
+console.log("finish");
+
+/*
 /////////////////////////////////
 //PROMISES
 const promise = new Promise((resolve, reject) => {
@@ -46,3 +58,4 @@ promise
     console.log(user);
   })
   .catch((err) => console.log(err.message));
+*/
